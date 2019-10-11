0c7920618a2938b3b3465349100bb37cd9a17bc4
aosante <andres.osante@proximitycr.com>
2019-10-11 09:24:24 -0600

    finished example app

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,31 +1,50 @@
 import React from "react";
 
-const Article = () => {
+const Article = props => {
+  const {
+    title,
+    publishDate,
+    content,
+    author: {
+      fields: {
+        fullName,
+        avatar: {
+          fields: {
+            file: { url }
+          }
+        }
+      }
+    },
+    featuredImage: {
+      fields: {
+        file: { url: featuredUrl }
+      }
+    }
+  } = props.article.fields;
+
+  const date = new Date(publishDate);
+  const formattedDate = `${date.getMonth()}-${date.getDay()}-${date.getFullYear()}`;
+  console.log(featuredUrl);
   return (
     <article className="post">
       <div className="post-preview col-xs-10 no-gutter">
         <h2 className="">
-          <a href="/hello-world/">Hello World</a>
+          <a href="/hello-world/">{title}</a>
         </h2>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero
-          sequi, labore incidunt amet qui corporis voluptate sint excepturi
-          error sed minus rerum aperiam cum possimus modi rem, tempora vero
-          ratione!
-        </p>
-
+        <p>{content}</p>
+        <img
+          width="550"
+          style={{ padding: "20px 0px" }}
+          src={featuredUrl}
+          alt=""
+        />
         <p className="meta">
-          <img
-            width="100"
-            src="https://png.pngtree.com/png-vector/20190411/ourmid/pngtree-business-male-icon-vector-png-image_916468.jpg"
-            alt=""
-            className="user-icon user-1"
-          />
+          <img width="50" src={url} alt="" className="user-icon user-1" />
           <a href=".author/andres/" className="mr-3">
-            Andres Osante
+            {fullName}
           </a>
           <time className="post-date" dateTime="2019-11-11">
-            Nov 2019
+            {formattedDate}
           </time>
         </p>
       </div>
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Article from "./Article";
 const contentful = require("contentful");
 
 const ArticleList = () => {
+  const [articles, setArticles] = useState([]);
   useEffect(() => {
     const client = contentful.createClient({
       // This is the space ID. A space is like a project folder in Contentful terms
@@ -10,16 +11,16 @@ const ArticleList = () => {
       // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
       accessToken: "9kXQcDAVrtyzxBajkKusE-oA5sGc9ZKz8NPUjrjB6IQ"
     });
-    client.getEntries({ content_type: "post" }).then(res => console.log(res));
+    client.getEntries({ content_type: "post" }).then(res => {
+      setArticles(res.items);
+    });
   }, []);
-  const articles = [];
-  for (let i = 0; i < 6; i++) {
-    articles.push(<Article key={i} />);
-  }
   return (
     <React.Fragment>
-      <h1>Article List</h1>
-      {articles}
+      <h1 style={{ textAlign: "center" }}>Surfing Spots</h1>
+      {articles.map((article, i) => (
+        <Article article={article} key={i} />
+      ))}
     </React.Fragment>
   );
 };
